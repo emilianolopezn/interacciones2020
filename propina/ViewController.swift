2a996ca6979257a28e0fff93c3c34620f91b52ef
José Trejo <joseatrejos>
2020-10-05 13:30:10 -0700

    Initial Commit

---
+++ propina/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  propina
+//
+//  Created by Alumno on 10/5/20.
+//  Copyright © 2020 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
